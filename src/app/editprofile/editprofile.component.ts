0797dce0c0e8342d53faa2a0b8b5674ec801ee07
Raghvender <raghvender002@gmail.com>
2021-04-12 16:49:12 +0530

    Added Referrals on Profile page

--- src/app/editprofile/editprofile.component.ts
+++ src/app/editprofile/editprofile.component.ts
@@ -1,97 +1,92 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ApiService } from 'src/services/api.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { UrlService } from 'src/services/url.service';
-import { CommonService } from 'src/services/common.service';
+import { Component, OnInit, AfterViewInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { ApiService } from "src/services/api.service";
+import { ActivatedRoute, Router } from "@angular/router";
+import { UrlService } from "src/services/url.service";
+import { CommonService } from "src/services/common.service";
 
 @Component({
-  selector: 'app-editprofile',
-  templateUrl: './editprofile.component.html',
-  styleUrls: ['./editprofile.component.scss']
+  selector: "app-editprofile",
+  templateUrl: "./editprofile.component.html",
+  styleUrls: ["./editprofile.component.scss"],
 })
 export class EditprofileComponent implements OnInit, AfterViewInit {
   //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
   //Add 'implements AfterViewInit' to the class.
 
-
-  imagePath: any
-  updateProfileForm: FormGroup
+  imagePath: any;
+  updateProfileForm: FormGroup;
   imageFile: any;
   sub: any;
   id: any;
-  profileData
+  profileData;
   submitted: boolean;
   firstname: any;
   lastName: any;
   address: any;
   countryCode: any;
   phone: any;
-  showProfilePic
-  progress: boolean = false
+  showProfilePic;
+  progress: boolean = false;
 
-  constructor(private fb: FormBuilder, private commonService: CommonService, private apiService: ApiService, private route: ActivatedRoute, private urlService: UrlService, private router: Router) {
-    this.sub = this.route
-      .queryParams
-      .subscribe(params => {
-        // Defaults to 0 if no query param provided.
-        this.id = params['id'];
-      });
-
+  constructor(
+    private fb: FormBuilder,
+    private commonService: CommonService,
+    private apiService: ApiService,
+    private route: ActivatedRoute,
+    private urlService: UrlService,
+    private router: Router
+  ) {
+    this.sub = this.route.queryParams.subscribe((params) => {
+      // Defaults to 0 if no query param provided.
+      this.id = params["id"];
+    });
 
     this.imagePath = this.urlService.imageUrl;
 
     this.apiService.getProfile().subscribe((res) => {
-      this.profileData = res.data
+      this.profileData = res.data;
 
-      this.firstname = this.profileData.firstName
-      this.lastName = this.profileData.lastName
-      this.countryCode = this.profileData.countryCode
-      this.phone = this.profileData.phone
-      this.showProfilePic = this.profileData.profilePic
+      this.firstname = this.profileData.firstName;
+      this.lastName = this.profileData.lastName;
+      this.countryCode = this.profileData.countryCode;
+      this.phone = this.profileData.phone;
+      this.showProfilePic = this.profileData.profilePic;
 
-      this.setValue()
+      this.setValue();
     });
-
   }
 
-
-
-  imagePreview: any
+  imagePreview: any;
   ngOnInit() {
-
     this.updateProfileForm = this.fb.group({
-      firstName: ['', [Validators.required, Validators.maxLength(25)]],
-      lastName: ['', [Validators.required, Validators.maxLength(25)]],
-      countryCode: ['', Validators.required],
-      phone: ['', Validators.required],
-      profilePic1: ['',],
-      profilePic: []
+      firstName: ["", [Validators.required, Validators.maxLength(25)]],
+      lastName: ["", [Validators.required, Validators.maxLength(25)]],
+      countryCode: ["", Validators.required],
+      phone: ["", Validators.required],
+      profilePic1: [""],
+      profilePic: [],
     });
-
-
-
   }
-  ngAfterViewInit(): void {
-
-
-  }
+  ngAfterViewInit(): void {}
 
   get f() {
-    return this.updateProfileForm.controls
+    return this.updateProfileForm.controls;
   }
 
   setValue() {
-    this.updateProfileForm.controls['firstName'].setValue(this.firstname)
-    this.updateProfileForm.controls['lastName'].setValue(this.lastName)
-    this.updateProfileForm.controls['countryCode'].setValue(this.countryCode)
-    this.updateProfileForm.controls['phone'].setValue(this.phone)
-    this.updateProfileForm.controls['profilePic1'].setValue(this.showProfilePic)
+    this.updateProfileForm.controls["firstName"].setValue(this.firstname);
+    this.updateProfileForm.controls["lastName"].setValue(this.lastName);
+    this.updateProfileForm.controls["countryCode"].setValue(this.countryCode);
+    this.updateProfileForm.controls["phone"].setValue(this.phone);
+    this.updateProfileForm.controls["profilePic1"].setValue(
+      this.showProfilePic
+    );
   }
 
-  picUploader: boolean
+  picUploader: boolean;
   async profilePic(event) {
-    this.picUploader = true
+    this.picUploader = true;
     if (event.target.files && event.target.files[0]) {
       this.imageFile = event.target.files[0];
       var reader = new FileReader();
@@ -99,43 +94,41 @@ export class EditprofileComponent implements OnInit, AfterViewInit {
       reader.onload = (event: any) => {
         this.imagePreview = event.target.result;
 
-        this.updateProfileForm.controls['profilePic1'].patchValue(this.imageFile);
+        this.updateProfileForm.controls["profilePic1"].patchValue(
+          this.imageFile
+        );
       };
     }
   }
 
   submit() {
-
-
-    this.submitted = true
+    this.submitted = true;
     if (this.submitted && this.updateProfileForm.valid) {
       const data = new FormData();
       if (this.picUploader === true) {
-        data.append('profilePic', this.imageFile, this.imageFile.name)
+        data.append("profilePic", this.imageFile, this.imageFile.name);
       }
-      data.append('firstName', this.updateProfileForm.get('firstName').value),
-        data.append('lastName', this.updateProfileForm.get('lastName').value),
-        data.append('email', this.profileData.email),
-        data.append('countryCode', this.updateProfileForm.get('countryCode').value),
-        data.append('phone', this.updateProfileForm.get('phone').value),
-        this.progress = true
+      data.append("firstName", this.updateProfileForm.get("firstName").value),
+        data.append("lastName", this.updateProfileForm.get("lastName").value),
+        data.append("email", this.profileData.email),
+        data.append(
+          "countryCode",
+          this.updateProfileForm.get("countryCode").value
+        ),
+        data.append("phone", this.updateProfileForm.get("phone").value),
+        (this.progress = true);
       this.apiService.updateProfile(data).subscribe((res) => {
         if (res.success) {
-          this.progress = false
-          this.commonService.successToast(res.message)
-          this.router.navigate(['profile']);
+          this.progress = false;
+          this.commonService.successToast(res.message);
+          this.router.navigate(["profile"]);
         } else {
-          this.progress = false
-          this.commonService.errorToast(res.message)
+          this.progress = false;
+          this.commonService.errorToast(res.message);
         }
-
       });
     } else {
-      console.log("invalid")
-
+      console.log("invalid");
     }
   }
-
-
-
 }
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,34 +1,99 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { ApiService } from 'src/services/api.service';
-import { UrlService } from 'src/services/url.service';
+import { Component, OnInit, AfterViewInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { ApiService } from "src/services/api.service";
+import { UrlService } from "src/services/url.service";
+import { CommonService } from "src/services/common.service";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  selector: "app-profile",
+  templateUrl: "./profile.component.html",
+  styleUrls: ["./profile.component.scss"],
 })
 export class ProfileComponent implements OnInit {
-
-  imageUrl: any
-  constructor(private router: Router, private apiService: ApiService, private urlService: UrlService) {
-
-  }
+  sub: any;
+  source: any;
+  receiver: any;
+  updateProfileForm: FormGroup;
+  progress: boolean = false;
+  submitted: boolean;
   profileData: any;
+  imageUrl: any;
+  id: any;
 
-  ngOnInit() {
-    this.imageUrl = this.urlService.imageUrl
-    this.apiService.getProfile().subscribe((res) => {
-      console.log("profile Data", res.data)
-      this.profileData = res.data
+  showProfilePic;
 
+  constructor(
+    private fb: FormBuilder,
+    private commonService: CommonService,
+    private router: Router,
+    private apiService: ApiService,
+    private urlService: UrlService,
+    private route: ActivatedRoute
+  ) {
+    this.sub = this.route.queryParams.subscribe((params) => {
+      // Defaults to 0 if no query param provided.
+      this.id = params["id"];
     });
 
+    this.apiService.getProfile().subscribe((res) => {
+      this.profileData = res.data;
+      this.source = this.profileData.source;
+      this.receiver = this.profileData.receiver;
+      this.setValue();
+    });
   }
-  goToeditprofile() {
-    
-   
-    //  
-    this.router.navigate(['editprofile'])
 
+  ngOnInit() {
+    this.imageUrl = this.urlService.imageUrl;
+    this.apiService.getProfile().subscribe((res) => {
+      console.log("profile Data", res.data);
+      this.profileData = res.data;
+    });
+
+    // Validator for Referral
+    this.updateProfileForm = this.fb.group({
+      source: ["", [Validators.required, Validators.max(100)]],
+      receiver: ["", [Validators.required, Validators.max(100)]],
+    });
+  }
+
+  get f() {
+    return this.updateProfileForm.controls;
+  }
+
+  // Edit Profile details Route
+  goToeditprofile() {
+    this.router.navigate(["editprofile"]);
+  }
+
+  // Set initial values in the Input
+  setValue() {
+    this.updateProfileForm.controls["source"].setValue(this.source);
+    this.updateProfileForm.controls["receiver"].setValue(this.receiver);
+  }
+
+  // Update Referral Codes
+  updateRef() {
+    this.submitted = true;
+    if (this.submitted && this.updateProfileForm.valid) {
+      const data = new FormData();
+
+      data.append("source", this.updateProfileForm.get("source").value),
+        data.append("receiver", this.updateProfileForm.get("receiver").value),
+        (this.progress = true);
+      this.apiService.updateProfile(data).subscribe((res) => {
+        if (res.success) {
+          this.progress = false;
+          this.commonService.successToast(res.message);
+          this.router.navigate(["profile"]);
+        } else {
+          this.progress = false;
+          this.commonService.errorToast(res.message);
+        }
+      });
+    } else {
+      console.log("invalid");
+    }
   }
 }
