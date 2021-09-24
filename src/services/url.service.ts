9bd6df8afb7348ef6ef5302b8bff9fb0522e4db2
Enamul Haque <enamul@apptunix.com>
2021-09-24 14:47:32 +0530

    base url replced

--- src/services/url.service.ts
+++ src/services/url.service.ts
@@ -5,10 +5,10 @@ import { Injectable } from "@angular/core";
 })
 export class UrlService {
   baseUrl = "";
- imageUrl = "https://test.webdevelopmentsolution.net:9085/";
-  SERVER_URL = "https://test.webdevelopmentsolution.net:9085"
-  // imageUrl = "https://api.tehbola.my"
-  // SERVER_URL = "https://api.tehbola.my"
+//  imageUrl = "https://test.webdevelopmentsolution.net:9085/";
+//   SERVER_URL = "https://test.webdevelopmentsolution.net:9085"
+  imageUrl = "https://api.tehbola.my/"
+  SERVER_URL = "https://api.tehbola.my"
 
   constructor() { }
   login = `${this.baseUrl}/login`;
